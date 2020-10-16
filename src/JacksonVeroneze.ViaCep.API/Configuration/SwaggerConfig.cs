f0e96d6c1e6520bacf313dd8007ff0280cd5f723
Jackson Veroneze <jackson@jacksonveroneze.com>
2020-10-16 09:54:36 -0300

    Criado estrutura (Controladores, swagger).

---
+++ src/JacksonVeroneze.ViaCep.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,37 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace JacksonVeroneze.ViaCep.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "VIACEP - Consulta de CEPs",
+                    Description = "VIACEP - Consulta de CEPs.",
+                    Contact = new OpenApiContact() { Name = "Jackson Veroneze", Email = "jackson@jacksonveroneze.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/JacksonVeroneze.ViaCep.API/Controllers/SearchPostalCode.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JacksonVeroneze.ViaCep.API.Controllers
+{
+
+    [ApiController]
+    [Route("search-cep")]
+    public class SearchPostalCode : ControllerBase
+    {
+        private readonly ILogger<SearchPostalCode> _logger;
+
+        //
+        // Summary:
+        //     /// Method responsible for initializing the controller. ///
+        //
+        // Parameters:
+        //   logger:
+        //     The logger param.
+        //
+        public SearchPostalCode(ILogger<SearchPostalCode> logger)
+        {
+            _logger = logger;
+        }
+
+        //
+        // Summary:
+        //     /// Method responsible for action: Get (GET). ///
+        //
+        [HttpGet("{numero}")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Get))]
+        public ActionResult Get(int numero)
+        {
+            _logger.LogInformation("Request: {0}", "Solicitado cálculo de juros.");
+
+            return Ok("ok");
+        }
+    }
+}
