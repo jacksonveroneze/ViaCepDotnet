4c3050e56657a4bd97586e9ed4aac2f964d11dea
Jackson Veroneze <jackson@jacksonveroneze.com>
2020-10-16 15:16:47 -0300

    Refatorado service para validar o cep e adicionado suporta a CORS.

---
+++ src/JacksonVeroneze.ViaCep.BuildingBlocks/BaseEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace JacksonVeroneze.ViaCep.BuildingBlocks
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; private set; }
+        
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; } = null;
+
+        public DateTime? DeletedAt { get; set; } = null;
+
+        public int Version { get; set; } = 1;
+
+        //
+        // Summary:
+        //     /// Method responsible for initializing the entity. ///
+        //
+        public BaseEntity() => CreatedAt = DateTime.Now;
+
+        //
+        // Summary:
+        //     /// Method responsible for updating the entity. ///
+        //
+        public void IncrementVersion() => Version++;
+
+        //
+        // Summary:
+        //     /// Method responsible for returning a string 
+        //     representation of the object. ///
+        //
+        public override string ToString()
+            => $"CreatedAt: {CreatedAt}, UpdatedAt: {UpdatedAt}, DeletedAt: {DeletedAt}, Version: {Version}";
+    }
+}
